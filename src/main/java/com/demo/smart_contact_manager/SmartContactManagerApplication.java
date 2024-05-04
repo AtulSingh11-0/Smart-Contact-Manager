2ccf59b05fb6d39445f310d400ce5072b0586024
Atul Singh <cocatul11@gmail.com>
2024-05-04 23:08:07 +0530

    chore: Refactor code formatting in test and main application files

--- src/main/java/com/demo/smart_contact_manager/SmartContactManagerApplication.java
+++ src/main/java/com/demo/smart_contact_manager/SmartContactManagerApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartContactManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartContactManagerApplication.class, args);
-	}
+  public static void main ( String[] args ) {
+    SpringApplication.run(SmartContactManagerApplication.class, args);
+    System.out.println("Smart Contact Manager Application Started... on http://localhost:1234/");
+  }
 
 }
