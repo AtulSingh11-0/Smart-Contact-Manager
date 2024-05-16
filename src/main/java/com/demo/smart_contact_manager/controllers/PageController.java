d01c5505e9ece704d3136b1560d65cd9c9acd4c7
Atul Singh <cocatul11@gmail.com>
2024-05-16 22:22:38 +0530

    chore: Update PageController with sign-up and login functionality
    This commit adds sign-up and login functionality to the PageController. The `signUp` method handles the GET request for the "/sign-up" endpoint and sets up the necessary model attributes for the "signUp" template. The `doSignUp` method handles the POST request for the "/do-signUp" endpoint and saves the user form data using the UserService. The `login` method handles the GET request for the "/login" endpoint and sets up the necessary model attributes for the "login" template. These changes enhance the user experience by allowing users to sign up and log in to the Smart Contact Manager application.

--- src/main/java/com/demo/smart_contact_manager/controllers/PageController.java
+++ src/main/java/com/demo/smart_contact_manager/controllers/PageController.java
@@ -1,11 +1,22 @@
 package com.demo.smart_contact_manager.controllers;
 
+import com.demo.smart_contact_manager.request.UserForm;
+import com.demo.smart_contact_manager.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Slf4j
 @Controller
 public class PageController {
+
+  @Autowired
+  private UserService userService;
 
   @RequestMapping("/")
   public String home( Model model ) {
@@ -27,5 +38,32 @@ public class PageController {
     model.addAttribute("githubRepoLink", "https://github.com/AtulSingh11-0");
 
     return "about";
+  }
+
+  @GetMapping("/sign-up")
+  public String signUp( Model model ) {
+
+    UserForm form = new UserForm();
+    model.addAttribute("pageTitle", "Sign Up - Smart Contact Manager");
+    model.addAttribute("userForm", form);
+
+    return "signUp";
+  }
+
+  @RequestMapping(value = "/do-signUp", method = RequestMethod.POST)
+  public String doSignUp( @ModelAttribute("userForm") UserForm userForm ) {
+
+    log.info("User form: {}", userForm.toString());
+    userService.saveUser(userForm);
+
+    return "redirect:/sign-up";
+  }
+
+  @RequestMapping("/login")
+  public String login( Model model ) {
+
+    model.addAttribute("pageTitle", "Login - Smart Contact Manager");
+
+    return "login";
   }
 }
